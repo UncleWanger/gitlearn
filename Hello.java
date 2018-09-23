bcd163f301d0a79882012bfabd60b3c82c0dfe84
UncleWanger <514553543@qq.com>
2018-09-24 07:52:14 +0800

    kk

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello{
 	public staic void main(String... args){
 		System.out.println("hello, man"); 
 		
-		System.out.println("world, www.hello.cn");
+		System.out.println("world, www.hello.jerry zhang yufei");
 		}
 	}
